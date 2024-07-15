43183c4f58b2ef4a27b00abb60a6dc4b98e12ee5
navrat22 <navrat22@seznam.cz>
2024-07-15 14:24:38 +0100

    Add files via upload

---
+++ Comparing_names task.py
@@ -0,0 +1,9 @@
+print("Enter your name:")
+first_name = input()
+print("Enter your middle name:")
+second_name = input()
+
+if first_name == second_name:
+    print("Same")
+else:
+    print("Different")
